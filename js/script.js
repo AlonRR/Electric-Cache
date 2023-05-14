ce8f7e433e5892f0c07cae096c2e339c5277a27f
YasminAdler <adleryasmin@gmail.com>
2023-05-14 07:45:03 +0300

    adding bootstrap

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var myModal = new bootstrap.Modal(document.getElementById('recommendedModal'));
+document.getElementById('setRecommendedButton').addEventListener('click', function () {
+    myModal.show();
+});
