4d0366536322cb2470344fca14be8ebe7b4df68b
YasminAdler <adleryasmin@gmail.com>
2023-05-15 16:43:10 +0300

    php

---
+++ param_to_send.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>get parameters PHP</title>
+</head>
+<body>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    // Retrieve the values of userName, password, and clue from the query string
+    $userName = $_GET["name"];
+    $battery_percent = $_GET["battery-percent"];
+    $start_time = $_GET["start-time"];
+    $end_time = $_GET["end-time"];
+    $frequency = $_GET["frequency"];
+    $vehicles = $_GET["vehicles[]"];
+
+    // Display the received values
+    echo "Hello: " . $userName . "<br>";
+    echo "battery-percent" . $battery_percent ."<br>";
+    echo "start-timet" . $start_timet ."<br>";
+    echo "end-time" . $end_time ."<br>";
+    echo "frequency" . $frequency. "<br>";
+    echo "vehicles[]" . $vehicles ."<br>";
+    
+}
+?>
+
+
+</body>
+</html>
