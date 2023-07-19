02f2099041f7970a406019ca7e3997da31b3154b
YasminAdler <adleryasmin@gmail.com>
2023-07-19 18:36:48 +0300

    icons changing

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 if (isset($_SESSION['username'])) {
-  $username = $_SESSION['username'];
-//   echo "Welcome, " . $username . "!";
+    $username = $_SESSION['username'];
+    //   echo "Welcome, " . $username . "!";
 } else {
-  header("Location: login.php"); // Redirect to the login page if not logged in
+    header("Location: login.php"); // Redirect to the login page if not logged in
 }
 ?>
 
@@ -55,15 +55,31 @@ if (isset($_SESSION['username'])) {
                         </li>
                         <li class="nav-item">
                             <a class="nav-link text-white" href="battery.html"> <!-- Added class: text-white -->
-                                <img src="images/Electric-Cache_Project_Battery_Charging_Full_Icon_1.png"
-                                    alt="Electric-Cache_Project_Battery_Charging_Full_Icon_1.png"
-                                    title="Electric-Cache_Project_Battery_Charging_Full_Icon_1.png">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="0.875em"
+                                    viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                    <style>
+                                        svg {
+                                            fill: #f5f5f5
+                                        }
+                                    </style>
+                                    <path
+                                        d="M80 96c0-17.7 14.3-32 32-32h64c17.7 0 32 14.3 32 32l96 0c0-17.7 14.3-32 32-32h64c17.7 0 32 14.3 32 32h16c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V160c0-35.3 28.7-64 64-64l16 0zm304 96c0-8.8-7.2-16-16-16s-16 7.2-16 16v32H320c-8.8 0-16 7.2-16 16s7.2 16 16 16h32v32c0 8.8 7.2 16 16 16s16-7.2 16-16V256h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H384V192zM80 240c0 8.8 7.2 16 16 16h96c8.8 0 16-7.2 16-16s-7.2-16-16-16H96c-8.8 0-16 7.2-16 16z" />
+                                </svg>
                                 Battery
                             </a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link text-white" href="stats.html"> <!-- Added class: text-white -->
-                                <img src="images/Electric-Cache_Project_Poll_Icon_1.png" alt="">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="0.875em"
+                                    viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                    <style>
+                                        svg {
+                                            fill: #f5f5f5
+                                        }
+                                    </style>
+                                    <path
+                                        d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm64 192c17.7 0 32 14.3 32 32v96c0 17.7-14.3 32-32 32s-32-14.3-32-32V256c0-17.7 14.3-32 32-32zm64-64c0-17.7 14.3-32 32-32s32 14.3 32 32V352c0 17.7-14.3 32-32 32s-32-14.3-32-32V160zM320 288c17.7 0 32 14.3 32 32v32c0 17.7-14.3 32-32 32s-32-14.3-32-32V320c0-17.7 14.3-32 32-32z" />
+                                </svg>
                                 Stats
                             </a>
                         </li>
