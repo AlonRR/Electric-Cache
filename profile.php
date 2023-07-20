f0f9fb8ebc3ee0bba1b893f6ebd31923d94496df
YasminAdler <adleryasmin@gmail.com>
2023-07-20 13:39:40 +0300

    still login

---
+++ profile.php
@@ -0,0 +1,91 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" type="text/css" href="#" />
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.2.2/Chart.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&family=Nunito+Sans:wght@200&family=Rubik&display=swap"
+        rel="stylesheet">
+    <link rel="icon" href="images/icon.ico">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/login.css">
+
+    <title>Electric-Cache</title>
+</head>
+
+<body class="body1">
+    <div class="container-fluid div1">
+        <header class="container p-3 ms-0 me-0 mw-100 bg-2b2b2b fw-300">
+            <nav class="navbar navbar-expand p-0 w-100">
+                <img src="images/freida-user.jpg" alt="user" class="userPic">
+                <div class="row p-0">
+                    <ul class="navbar-nav align-items-end fw-900 fs-30">
+                        <li class="nav-item">
+                            <a class="row align-items-end navbar-brand" href="index.php">
+                                <div class="col logo94"></div>
+                                <h1 class="col display-5 justify-content-end align-bottom p-0 mb-0 text-white">
+                                    Electric-Cache
+                                </h1>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="index.php"> <!-- Added class: text-white -->
+                                <img src="images/Electric-Cache_Project_Home_Icon_1.png"
+                                    alt="Electric-Cache_Project_Home_Icon_1.png">
+                                Home
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="battery.html"> <!-- Added class: text-white -->
+                                <svg xmlns="http://www.w3.org/2000/svg" height="0.875em"
+                                    viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                    <style>
+                                        svg {
+                                            fill: #f5f5f5
+                                        }
+                                    </style>
+                                    <path
+                                        d="M80 96c0-17.7 14.3-32 32-32h64c17.7 0 32 14.3 32 32l96 0c0-17.7 14.3-32 32-32h64c17.7 0 32 14.3 32 32h16c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V160c0-35.3 28.7-64 64-64l16 0zm304 96c0-8.8-7.2-16-16-16s-16 7.2-16 16v32H320c-8.8 0-16 7.2-16 16s7.2 16 16 16h32v32c0 8.8 7.2 16 16 16s16-7.2 16-16V256h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H384V192zM80 240c0 8.8 7.2 16 16 16h96c8.8 0 16-7.2 16-16s-7.2-16-16-16H96c-8.8 0-16 7.2-16 16z" />
+                                </svg>
+                                Battery
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="stats.html"> <!-- Added class: text-white -->
+                                <svg xmlns="http://www.w3.org/2000/svg" height="0.875em"
+                                    viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                    <style>
+                                        svg {
+                                            fill: #f5f5f5
+                                        }
+                                    </style>
+                                    <path
+                                        d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm64 192c17.7 0 32 14.3 32 32v96c0 17.7-14.3 32-32 32s-32-14.3-32-32V256c0-17.7 14.3-32 32-32zm64-64c0-17.7 14.3-32 32-32s32 14.3 32 32V352c0 17.7-14.3 32-32 32s-32-14.3-32-32V160zM320 288c17.7 0 32 14.3 32 32v32c0 17.7-14.3 32-32 32s-32-14.3-32-32V320c0-17.7 14.3-32 32-32z" />
+                                </svg>
+                                Stats
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active text-white" aria-current="page" href="listPage.html">
+                                <img src="images/Electric-Cache_Project_List_Alt_Icon_1.png" alt="">
+                                Template
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+</body>
+
+</html>
